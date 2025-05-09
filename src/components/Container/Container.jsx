79b170aa5726e12b96d2e1db7cc1db0e186a9de3
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-09 23:59:14 +0400

    Commit

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -5,8 +5,10 @@ import Equilibrium from "../../../public/images/image-equilibrium.jpg"
 export default function Container() {
   return (
     <div className='container'>
-      <img className="equilibrium" src={Equilibrium} alt="" />
-      <Content />
+      <div className='equilibrium-container'>
+        <img className="equilibrium" src={Equilibrium} alt="" />
+      </div>
+        <Content /> 
     </div>
   )
 }
